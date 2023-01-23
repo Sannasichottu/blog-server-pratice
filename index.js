b9d106126932b20056c813e01e41e3e7912a8abc
Sannasi <sannasichottu@gmail.com>
2023-01-24 01:00:53 +0530

    auth

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 require ('dotenv').config();
 const cors = require('cors');
 const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/users')
 
 
 
@@ -21,9 +22,13 @@ connection.once('open',()=>{
 
 //Middleware
 app.use(cors());
+app.use(express.json())
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+
+//routes
 app.use('/posts',postRoutes)
+app.use('/user',userRoutes)
 
 app.listen(5000, () => {
     console.log(`server is running`);
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
 const {getPosts,createPost,updatePost,deletePost,likePost}  = require('../controllers/posts')
 
 router.get('/', getPosts)
-router.post('/', createPost)
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost)
+router.post('/',auth, createPost)
+router.patch('/:id',auth, updatePost);
+router.delete('/:id',auth, deletePost);
+router.patch('/:id/likePost', auth, likePost)
 
 
 module.exports = router;
